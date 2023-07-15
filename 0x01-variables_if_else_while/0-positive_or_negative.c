511e9f8f6f331a3fd7f0b54a5d47f1dfaadab1d4
kwamesafoboateng <samuelkwamesafoboateng@gmail.com>
2023-07-14 19:23:11 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point writing a program that will assign random numbers to the variable
+ * main - Entry point
  * Return: 0 (succes)
  */
-
 int main(void)
 {
 	int n;
@@ -14,7 +13,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n== 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
