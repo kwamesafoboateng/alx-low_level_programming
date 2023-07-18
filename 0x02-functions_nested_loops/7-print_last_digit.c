c847148e832b14e946248c6201831726ebbfc678
kwamesafoboateng <samuelkwamesafoboateng@gmail.com>
2023-07-18 19:35:41 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,8 @@ int print_last_digit(int a)
 	int b;
 
 	b = a % 10;
-
-		if (a < 0)
-		{
-			b = -b;
-			_putchar(b + '0');
-		}
-			return (b);
+	if (b < 0)
+		b = -b;
+	_putchar(b + '0');
+	return (b);
 }
