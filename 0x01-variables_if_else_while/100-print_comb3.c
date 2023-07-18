47ce1dce9564ef8ddda72636df81e80789962a55
kwamesafoboateng <samuelkwamesafoboateng@gmail.com>
2023-07-18 22:51:46 +0000

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
+
 /**
-<<<<<<< HEAD
  * main - prints all possible different combinations of two digits
  * Return: ALways 0 (Success)
-=======
- * main - Entry point
- * Return: 0
->>>>>>> 739321149a6bfd874a9794b15eb3c06201a34fae
  */
+
 int main(void)
 {
-	int a, b;
+	int n, m;
 
-<<<<<<< HEAD
-	for (a = 48; n < 58
-=======
-	for (a = 48; a <= 56; a++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (a = 49; b <= 57; b++)
+		for (m = 49; m <= 57; m++)
 		{
-			if (b > a)
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
 				{
-					putchar(a);
-					putchar(b);
-					if (a != 56 || b != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
+			}
 		}
 	}
 	putchar('\n');
-return (0);
+	return (0);
 }
->>>>>>> 739321149a6bfd874a9794b15eb3c06201a34fae
