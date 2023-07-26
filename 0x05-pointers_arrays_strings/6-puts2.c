80f317102ce54db73c596f7e3d84af8bf9b2c385
kwamesafoboateng <samuelkwamesafoboateng@gmail.com>
2023-07-26 18:34:44 +0000

    puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -20,12 +20,13 @@ void puts2(char *str)
 		longi++;
 	}
 	a = longi - 1;
+
 	for (o = 0 ; o <= a ; o++)
-			{
-			if (o % 2 == 0)
-			{
+	{
+		if (o % 2 == 0)
+		{
 			_putchar(str[o]);
-			}
-			}
+		}
+	}
 	_putchar('\n');
 }
