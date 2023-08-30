6d8aec2cc85abb672dde7927506bf23766ef4d2b
kwamesafoboateng <samuelsafoboateng90@gmail.com>
2023-08-30 00:25:41 +0000

    updated

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * my_function - prints a sentence before main
+ * function is executed
+ * 
+ */
+
+void my_function(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
