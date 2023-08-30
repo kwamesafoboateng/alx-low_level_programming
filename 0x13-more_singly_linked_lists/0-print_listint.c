314da824207230c12b43c7946f2a8fcb363618b0
kwamesafoboateng <samuelsafoboateng90@gmail.com>
2023-08-30 21:22:47 +0000

    updated

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+
+
+size_t print_listint(const listint_t *h)
+{
+    size_t node_cnt = 0;
+
+    while (h != NULL)
+    {
+        printf("%d", h->n);
+
+        if (h->next != NULL)
+            printf(", ");
+
+        h = h->next;
+        node_cnt++;
+    }
+
+    printf("\n");
+    return node_cnt;
+}
