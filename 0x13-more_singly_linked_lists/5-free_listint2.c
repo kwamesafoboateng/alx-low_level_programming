58cff8e644d7b5b387b3480c5bc83699e79f786e
kwamesafoboateng <samuelsafoboateng90@gmail.com>
2023-09-01 00:31:26 +0000

    updated

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list
+ * @head: A pointer to the address of the
+ *        head of the listint_t list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *cnt;
+
+	while (*head != NULL)
+	{
+	cnt = (*head)->next;
+	free(*head);
+	*head = cnt;
+	}
+	*head = NULL;
+}
