07879137ed90940031f2032db35a0d7f8699dd6d
nanakwamesafo <samuelkwamesafoboateng@gmail.com>
2023-10-30 20:25:14 +0000

    file descriptor

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t op;
+	ssize_t wr;
+	ssize_t rd;
+
+	op = open(filename, O_RDONLY);
+	if (op == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	rd = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	free(buffer);
+	close(op);
+	return (wr);
+}
